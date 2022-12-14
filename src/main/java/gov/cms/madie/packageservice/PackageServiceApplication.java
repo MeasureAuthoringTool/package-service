c50dd5dd3041ca40b086f14f2778328bb1a8f6ab
adongare <ashok.dongare@semanticbits.com>
2022-12-14 12:08:52 -0500

    MAT-5120 added spring security with okta oauth

--- src/main/java/gov/cms/madie/packageservice/PackageServiceApplication.java
+++ src/main/java/gov/cms/madie/packageservice/PackageServiceApplication.java
@@ -1,12 +1,43 @@
 package gov.cms.madie.packageservice;
 
+import gov.cms.madie.packageservice.config.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PackageServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PackageServiceApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer(@Autowired LogInterceptor logInterceptor) {
+    return new WebMvcConfigurer() {
+
+      @Override
+      public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(logInterceptor);
+      }
+
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("PUT", "POST", "GET")
+            .allowedOrigins(
+                "http://localhost:9000",
+                "https://dev-madie.hcqis.org",
+                "https://test-madie.hcqis.org",
+                "https://impl-madie.hcqis.org",
+                "https://madie.cms.gov/");
+      }
+    };
   }
 }
